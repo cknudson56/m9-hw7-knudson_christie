2e09091824b35a8fcf07192140ee5a7d27f17f02
Christie L Knudson <christie.knudson@Christies-MacBook-Pro.local>
2018-10-20 19:31:51 -0500

    Fix location menu, but still affecting other button

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
+//BUTTON HOVER
 var button = document.getElementsByClassName('hover-button');
 
 function hoverColor() {
 	this.classList.toggle('hover-color');
 };
 
-function showDiv() {
-	this.nextElementSibling.classList.toggle('show-div');
-//The only way I can get the drop nav is this but it also affects the other button.
-};
-
 for (i=0; i < button.length; i++) {
 	button[i].addEventListener('mouseenter', hoverColor);
 	button[i].addEventListener('mouseleave', hoverColor);
-	button[i].addEventListener('click', showDiv);
 };
 
-//var locationButton = document.getElementsByClassName('hover-button')[0];
-//var locationDiv = document.getElementsByClassName('hide-div');
 
-//function dropDownLocations() {
-//	locationDiv.classList.toggle("show-div");
-//};
-//locationButton.onclick = dropDownLocations;
 
+//LOCATION DROP MENU
+var locationButton = document.getElementById('location-button');
+var locationMenu = document.getElementById('location-menu');
+
+function dropDownLocations() {
+	locationMenu.classList.toggle('show-div');
+};
+//locationButton.addEventListener('click', dropDownLocations);
+locationButton.onclick = dropDownLocations;
+
+
+
+//GRID HOVER
 var imgItem = document.getElementsByClassName('flex-img');
 function opacImg() {
 	this.classList.toggle('opac-Img');
